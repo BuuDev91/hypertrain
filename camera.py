3f8eb1553ab8ef1d16257be667c395811b33c1f4
buu1991 <ra.bucher91@gmail.com>
2019-02-22 12:06:51 +0100

    init lap signal

--- camera.py
+++ camera.py
@@ -14,13 +14,6 @@ import imutils
 
 from tesserocr import PyTessBaseAPI, PSM
 from PIL import Image
-
-# initialize the camera and grab a reference to the raw camera capture
-#camera = PiCamera()
-#camera.resolution = (640, 480)
-#camera.framerate = 32
-#camera.rotation = 180
-#rawCapture = PiRGBArray(camera, size=(640, 480))
  
 vs = PiVideoStream(resolution=(480, 360), framerate=32).start()
 time.sleep(2)
@@ -28,24 +21,57 @@ fps = FPS().start()
 
 # define range HSV for white color of the sign
 sensitivity = 30
-lower_color = np.array([0,0,255-sensitivity])
-upper_color = np.array([255,sensitivity,255])
+lower_white_color = np.array([0, 0, 255-sensitivity])
+upper_white_color = np.array([255, sensitivity, 255])
+lower_black_color = np.array([0, 0, 0])
+upper_black_color = np.array([50, 50, sensitivity])
+lower_blue_color = np.array([120-sensitivity,50,50])
+upper_blue_color = np.array([120+sensitivity,255,255])
 
-def image_smoothening(img):
-    ret1, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    ret2, th2 = cv2.threshold(th1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    blur = cv2.GaussianBlur(th2, (1, 1), 0)
-    ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    return th3
+def analyzeArea(image, warped):
+		
+	amountOfBlue = getAmountOfBlue(warped)
+	if (amountOfBlue > 0.1):
+		cv2.putText(image, "Rundensignal", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
+		print(datetime.datetime.now().strftime("%H:%M:%S") + ": " + "Rundensignal")
+	else:
+		#optimized = warped[50:450, 50:450]
+		cropValue = 3
+		optimized = cv2.resize(warped, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+		optimized = optimized[cropValue:optimized.shape[1] - cropValue, cropValue:optimized.shape[0] - cropValue]
+		optimized = cv2.GaussianBlur(optimized, (5, 5), 0)
+		optimized = cv2.adaptiveThreshold(cv2.cvtColor(optimized, cv2.COLOR_BGR2GRAY), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+		
+		cv2.imshow("optimized", optimized)
 
-def remove_noise_and_smooth(img):
-    filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 41, 3)
-    kernel = np.ones((5, 5), np.uint8)
-    opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
-    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-    img = image_smoothening(img)
-    or_image = cv2.bitwise_or(img, closing)
-    return or_image
+		result_txt = ""
+		if True: #enable / disable ocr reading (slowing down)
+			with PyTessBaseAPI(psm=PSM.SINGLE_CHAR) as api:
+				api.SetVariable("classify_bln_numeric_mode", "1")
+				pil_image = Image.fromarray(optimized)
+				api.SetImage(pil_image)
+				result_txt = api.GetUTF8Text()
+
+		result_txt = result_txt.replace("\n", "")
+		result_txt = result_txt.replace(" ", "")
+		if result_txt.isdigit() and int(result_txt) < 5:
+			print(datetime.datetime.now().strftime("%H:%M:%S") + ": " + result_txt)
+			cv2.putText(image, str(result_txt), (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+
+def getAmountOfBlue(img):
+	rgbSensitivity = int(sensitivity*1.5)
+	blue = [rgbSensitivity, rgbSensitivity, 255-rgbSensitivity]  # RGB
+	boundaries = [([blue[2]-rgbSensitivity, blue[1]-rgbSensitivity, blue[0]-rgbSensitivity],
+				[blue[2]+rgbSensitivity, blue[1]+rgbSensitivity, blue[0]+rgbSensitivity])]
+	# in order BGR as opencv represents images as numpy arrays in reverse order
+
+	for (lower, upper) in boundaries:
+		lower = np.array(lower, dtype=np.uint8)
+		upper = np.array(upper, dtype=np.uint8)
+		mask = cv2.inRange(img, lower, upper)
+		ratio_blue = cv2.countNonZero(mask)/(img.size/3)
+
+	return ratio_blue
  
 # capture frames from the camera
 while True:
@@ -59,8 +85,15 @@ while True:
 
 	# define kernel for smoothing   
 	kernel = np.ones((3,3),np.uint8)
-	# extract binary image with active white regions
-	mask = cv2.inRange(hsv, lower_color, upper_color)
+
+	# extract binary image with defined color regions
+	maskWhite = cv2.inRange(hsv, lower_white_color, upper_white_color)
+	maskBlack = cv2.inRange(hsv, lower_black_color, upper_black_color)
+	maskBlue = cv2.inRange(hsv, lower_blue_color, upper_blue_color)
+
+	mask = cv2.bitwise_or(maskWhite, maskBlue)
+	#mask = cv2.bitwise_or(mask, maskBlack)
+
 	# morphological operations
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 	mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
@@ -78,12 +111,6 @@ while True:
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
 		for cnt in cnts:
-			# Rotated Rectangle. Here, bounding rectangle is drawn with minimum area,
-			# so it considers the rotation also. The function used is cv2.minAreaRect().
-			# It returns a Box2D structure which contains following detals -
-			# ( center (x,y), (width, height), angle of rotation ).
-			# But to draw this rectangle, we need 4 corners of the rectangle.
-			# It is obtained by the function cv2.boxPoints()
 			rect = cv2.minAreaRect(cnt)
 			x_y,l_w,angle = rect
 			if angle and (angle >= -90 and angle <= -60 or angle >= -30):
@@ -93,46 +120,26 @@ while True:
 				# count euclidian distance for each side of the rectangle
 				sideOne = np.linalg.norm(box[0]-box[1])
 				sideTwo = np.linalg.norm(box[0]-box[3])
+
 				# count area of the rectangle
 				area = sideOne*sideTwo
 				sideRatio = sideOne / sideTwo
 
-
-				# find the largest rectangle within all contours
-				if area > largestArea and sideRatio >= 0.8 and sideRatio <= 1.2:
+				# find all contours looking like a signal
+				#if area > largestArea and sideRatio >= 0.8 and sideRatio <= 1.2:
+				if area > 500 and sideRatio >= 0.8 and sideRatio <= 1.2:
 					cv2.drawContours(image,[box],0,(0,255,0),2)
-					largestArea = area
-					largestRect = box
-					largestAngle = angle
+					
+					warped = four_point_transform(image, [box][0])
+					analyzeArea(image, warped)
+
 		
-	if largestArea > frameArea*0.001:
+	if False and largestArea > frameArea*0.001:
 		# draw contour of the found rectangle on  the original image   
 		cv2.drawContours(image,[largestRect],0,(0,0,255),2)
-		
-		# cut and warp interesting area
+
 		warped = four_point_transform(image, [largestRect][0])
-		cv2.imshow("warped", warped)
-
-		resized = imutils.resize(warped, height=500)
-		gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-		smooth = remove_noise_and_smooth(gray)
-		#crop image
-		smooth = smooth[50:450, 50:450]
-		cv2.imshow("smooth", smooth)
-
-		result_txt = ""
-		if True: #enable / disable ocr reading (slowing down)
-			with PyTessBaseAPI(psm=10) as api:
-				api.SetPageSegMode
-				pil_image = Image.fromarray(smooth)
-				api.SetImage(pil_image)
-				result_txt = api.GetUTF8Text()
-		
-		#cv2.putText(image, "Angle: " + str(largestAngle) + " Ratio: " + str(sideRatio), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 0, 0), 2)
-		
-		if result_txt:
-			print(datetime.datetime.now().strftime("%H:%M:%S") + ": " + result_txt)
-			cv2.putText(image, str(result_txt), (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+		analyzeArea(image, warped)
 
 	cv2.imshow("image", image)
 
