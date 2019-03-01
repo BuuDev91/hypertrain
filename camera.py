98c84b29fe148ea249ebc01721fc993a3053ee19
buu1991 <ra.bucher91@gmail.com>
2019-03-01 12:16:31 +0100

    fixed countour finding, now analyzes a list of all contours within the image. optimized color and thus signal detection (lap or digit)

--- camera.py
+++ camera.py
@@ -1,51 +1,55 @@
 # import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import numpy as np
 import time
 import datetime
 import cv2
 import sys
-
+import imutils
 
 from imutils.video.pivideostream import PiVideoStream
 from imutils.video import FPS
 from imutils.perspective import four_point_transform
 from imutils import contours
-import imutils
-
 from tesserocr import PyTessBaseAPI, PSM
 from PIL import Image
  
-vs = PiVideoStream(resolution=(480, 360), framerate=32).start()
+vs = PiVideoStream(resolution=(480, 368), framerate=32).start()
 time.sleep(2)
 fps = FPS().start()
 
 # define range HSV for white color of the sign
-sensitivity = 40
-lower_white_color = np.array([0, 5, 200])
+lower_white_color = np.array([0, 15, 100])
 upper_white_color = np.array([180, 255, 255])
 
 lower_black_color = np.array([0, 0, 0])
-upper_black_color = np.array([180, 255, sensitivity])
+upper_black_color = np.array([180, 255, 40])
 
-lower_blue_color = np.array([100,225,100])
+lower_blue_color = np.array([100,200,0])
 upper_blue_color = np.array([120,255,255])
 
-def analyzeArea(image, warped):
-		
-	if (getAmountOfColor(warped, lower_blue_color, upper_blue_color) > 0.03): #tbd
-		cv2.putText(image, "Rundensignal", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
-		print(datetime.datetime.now().strftime("%H:%M:%S") + ": " + "Rundensignal")
-	elif (getAmountOfColor(warped, lower_black_color, upper_black_color) > 0.1):
-		#optimized = warped[50:450, 50:450]
-		cropValue = 2
+KNOWN_WIDTH = 4.5
+KNOWN_DISTANCE = 50.0
+FOCAL_LENGTH = 0
+
+def analyzeArea(image, warped, box):
+
+	FOCAL_LENGTH = 3040.0 #calibrate: (box[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+	#print ("Focal length: " + str(FOCAL_LENGTH))
+
+	if (box[1][0] > 0.0):
+		DISTANCE = (KNOWN_WIDTH * FOCAL_LENGTH) / float(box[1][0])
+		#print ("Distance: " + str(DISTANCE))
+	
+	# find amount of color black in warped area, assuming over X% is a numeric signal
+	if (getAmountOfColor(warped, lower_black_color, upper_black_color) > 0.4):
+		print ("amount of black: " + str(getAmountOfColor(warped, lower_black_color, upper_black_color)))
+
+		# cropValue: amount of the frame to be cropped out
+		cropValue = 10
 		optimized = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-		optimized = cv2.resize(optimized, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+		optimized = cv2.resize(optimized, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
 		optimized = optimized[cropValue:optimized.shape[1] - cropValue, cropValue:optimized.shape[0] - cropValue]
-		#optimized = cv2.GaussianBlur(optimized, (5, 5), 0)
-		#optimized = cv2.adaptiveThreshold(optimized, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-		
+		optimized = cv2.GaussianBlur(optimized, (5, 5), 0)
 		cv2.imshow("optimized", optimized)
 
 		result_txt = ""
@@ -62,36 +66,38 @@ def analyzeArea(image, warped):
 			print(datetime.datetime.now().strftime("%H:%M:%S") + ": " + result_txt)
 			cv2.putText(image, str(result_txt), (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
 
-def getAmountOfColor(img, lowerColor, upperColor):
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	# find amount of color blue in warped area, assuming over X% is the lap signal
+	elif (getAmountOfColor(warped, lower_blue_color, upper_blue_color) > 0.2):
+		print ("amount of blue: " + str(getAmountOfColor(warped, lower_blue_color, upper_blue_color)))
+		cv2.putText(image, "Rundensignal", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
+		print(datetime.datetime.now().strftime("%H:%M:%S") + ": " + "Rundensignal")
+
+def getAmountOfColor(img, lowerColor, upperColor, convert2hsv = True):
+	if (convert2hsv):
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 	maskColor = cv2.inRange(img, lowerColor, upperColor)
 	ratio_blue = cv2.countNonZero(maskColor)/(img.size/3)
 
 	#print("Ratio: " + str(ratio_blue))
 	return ratio_blue
 
+#color picker for manual debugging color HSV range
 def pick_color(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         pixel = hsv[y,x]
 
-        upper =  np.array([pixel[0], pixel[1], pixel[2]])
-        lower =  np.array([pixel[0], pixel[1], pixel[2]])
-        print(lower, upper)
-
-        image_mask = cv2.inRange(hsv,lower,upper)
-        cv2.imshow("mask",image_mask)
+        color =  np.array([pixel[0], pixel[1], pixel[2]])
+        print(color)
 
 global hsv, pixel
  
 # capture frames from the camera
 while True:
-
 	image = vs.read()
 	image = imutils.rotate(image, angle=180)
 
 	frameArea = image.shape[0]*image.shape[1]
 	
-	#blurred = cv2.GaussianBlur(image, (11, 11), 0)
 	# convert color image to HSV color scheme
 	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
@@ -104,28 +110,40 @@ while True:
 	maskBlack = cv2.inRange(hsv, lower_black_color, upper_black_color)
 	maskBlue = cv2.inRange(hsv, lower_blue_color, upper_blue_color)
 
+	# combine color masks bitwise or
 	mask = cv2.bitwise_or(maskBlack, maskBlue)
 	mask = cv2.bitwise_or(mask, maskWhite)
 
-	#mask = cv2.erode(mask, None, iterations=2)
-	#mask = cv2.dilate(mask, None, iterations=2)
+	# erode and dilate masks, smoothing
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
 
 	# define kernel for smoothing   
 	kernel = np.ones((3,3),np.uint8)
 	# morphological operations
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 	mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+	# leave just the outlines of the contours:
+	# applying a gaussian blur and canny the outlines
+	mask = cv2.GaussianBlur(mask, (5, 5), 0)
+	mask = cv2.Canny(mask, 35, 125)
+
 	cv2.imshow("mask", mask)
 	
-	# find contours in the mask
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+	# get a list of contours in the mask, chaining to just endpoints
+	_,cnts,_ = cv2.findContours(mask.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
+		# loop contours
 		for cnt in cnts:
 			rect = cv2.minAreaRect(cnt)
-			x_y,l_w,angle = rect
-			if angle and (angle >= -90 and angle <= -60 or angle >= -30):
+			_,_,angle = rect
+			# approximate shape, if it has a length of 4 we assume its a rectangle
+			approx = cv2.approxPolyDP(cnt, 0.04 * cv2.arcLength(cnt, True), True)
+			# the rectangle must not have a too big rotation (+/-30°)
+			if len(approx) == 4 and angle and (angle >= -90 and angle <= -80 or angle >= -10):
 				box = cv2.boxPoints(rect)
 				box = np.int0(box)
 			
@@ -137,15 +155,12 @@ while True:
 				area = sideOne*sideTwo
 				sideRatio = sideOne / sideTwo
 
-				#cv2.drawContours(image,[box],0,(0,255,0),2)
-
-				# find all contours looking like a signal
-				#if area > largestArea and sideRatio >= 0.8 and sideRatio <= 1.2:
-				if area > 500 and sideRatio >= 0.8 and sideRatio <= 1.2:
-					print("Area: " + str(area))
-					cv2.drawContours(image,[box],0,(0,255,0),2)
+				# find all contours looking like a signal with minimum area
+				if area > 750 and sideRatio >= 0.8 and sideRatio <= 1.2:
+					print("Area: " + str(area) + " Angle: " + str(angle) + " SideRatio: " + str(sideRatio))
+					cv2.drawContours(image,[box],0,(0,255,0),1)
 					warped = four_point_transform(image, [box][0])
-					analyzeArea(image, warped)
+					analyzeArea(image, warped, box)
 
 	cv2.imshow("image", image)
 
